15068ecfde652bae2bea4354573ba5d6a8755a88
ksjadeja <kuldeepjadeja459@gmail.com>
2020-01-31 01:34:49 +0530

    add account entry mechanism working

--- app/src/main/java/com/journalapp/models/AccEntrybox.java
+++ app/src/main/java/com/journalapp/models/AccEntrybox.java
@@ -2,6 +2,62 @@ package com.journalapp.models;
 
 public class AccEntrybox {
 
+    private String name;
+    private String date;
 
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    private String time;
+    private float amount;
+    private String desc;
+    private static final int GIVE=0;
+    private static final int TAKE=1;
+    private int t_type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getT_type() {
+        return t_type;
+    }
+
+    public void setT_type(int t_type) {
+        this.t_type = t_type;
+    }
 
 }
